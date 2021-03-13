68ca458855323f68fb381a25f7528d3a7043a948
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-13 14:53:33 +0900

    wip:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ChakraProvider>
+        <Button>add</Button>
+      </ChakraProvider>
     </div>
   );
 }
