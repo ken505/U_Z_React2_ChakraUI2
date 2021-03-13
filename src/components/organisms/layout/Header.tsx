ed5902a62d60982746bf4fa01796dea3022bd509
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-13 16:58:42 +0900

    wip:

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,14 @@
+import { Flex } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
-  return <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+    ></Flex>
+  );
 });
