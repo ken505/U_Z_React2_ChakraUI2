c63b46bc77751bf0eb3f877d564c46905a262f5d
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-14 08:07:17 +0900

    71_done:

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,21 @@
 import { memo, VFC } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
-  return <p>Login</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" ws="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          User Management App
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="User ID" />
+          <PrimaryButton>Login</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
