c4ab267b1d5cb9e5b46b6d330bb183f34963d2dd
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-13 18:23:23 +0900

    befor_chakra_icon_storm:

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
@@ -9,6 +9,24 @@ export const Header: VFC = memo(() => {
       color="gray.50"
       align="center"
       justify="space-between"
-    ></Flex>
+      padding={{ base: 3, md: 5 }}
+    >
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          User Management App
+        </Heading>
+      </Flex>
+      <Flex
+        align="center"
+        fontSize="sm"
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+      >
+        <Box pr="4">
+          <Link>Users</Link>
+        </Box>
+        <Link>Config</Link>
+      </Flex>
+    </Flex>
   );
 });
