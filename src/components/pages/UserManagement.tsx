63cc3cae4fd50e69df31d1b1f13acdda229c50af
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-14 20:51:58 +0900

    wip:

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
 import { memo, VFC } from "react";
+import { Box, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>UserManagement</p>;
+  return (
+    <Wrap>
+      <WrapItem>
+        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md">
+          <Stack textAlign="center"></Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
