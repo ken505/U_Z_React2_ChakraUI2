70475adcc38f3d60244a0371bc367a2c8ee0e2c0
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-15 17:17:41 +0900

    79_done:

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -5,10 +5,12 @@ import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "../hooks/useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -20,16 +22,21 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            const isAdmin = res.data.id === 10 ? true : false;
+            setLoginUser({ ...res.data, isAdmin });
             showMessage({ title: "Login", status: "success" });
             history.push("/home");
           } else {
             showMessage({ title: "Not found", status: "error" });
+            setLoading(false);
           }
         })
-        .catch(() => showMessage({ title: "Unable to login", status: "error" }))
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: "Unable to login", status: "error" });
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
